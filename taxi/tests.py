7ad75a7cf5453bb9b9ec7867b3d26059aca8bdaf
Nikita Grinyov <nikita3grynov@gmail.com>
2025-12-16 11:05:47 +0200

    task

--- taxi/tests.py
+++ taxi/tests.py
@@ -1,3 +1,98 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from taxi.forms import DriverCreationForm, DriverSearchForm
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            username="admin",
+            password="testadmin",
+
+        )
+        self.client.force_login(self.admin_user)
+        self.driver = get_user_model().objects.create_user(
+            username="author",
+            password="testdriver",
+            license_number="MVL93412"
+        )
+
+    def test_driver_listed(self):
+        """
+        Test get driver's license number is in list_display on driver admin page.
+        :return:
+        """
+        url = reverse("admin:taxi_driver_changelist")
+        res = self.client.get(url)
+        self.assertContains(res, self.driver.license_number)
+
+    def test_driver_detail_pseudonym_listed(self):
+        """
+        Test get driver's license number is on driver detail admin page.
+        :return:
+        """
+        url = reverse("admin:taxi_driver_change", args=[self.driver.id])
+        res = self.client.get(url)
+        self.assertContains(res, self.driver.license_number)
+
+
+class FormsTests(TestCase):
+    def test_driver_creation_form(self):
+        form_data = {
+            "username": "driver1",
+            "password1": "user12test",
+            "password2": "user12test",
+            "first_name": "Test first",
+            "last_name": "Test last",
+            "license_number": "ABC99999"
+        }
+        form = DriverCreationForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data, form_data)
+
+
+TOGGLE_URL = reverse("taxi:toggle-car-assign",
+                     kwargs={"pk": 1})
+
+
+class PublicViewsTests(TestCase):
+    def test_toggle_assign_login_required(self):
+        response = self.client.post(TOGGLE_URL)
+        self.assertEqual(response.status_code, 302)
+        self.assertIn("/accounts/login/", response.url)
+
+
+class PrivateViewsTests(TestCase):
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+            username="test",
+            password="test123"
+        )
+        self.client.force_login(self.user)
+
+    def test_create_driver(self):
+        form_data = {
+            "username": "driver12",
+            "password1": "user12test",
+            "password2": "user12test",
+            "first_name": "Test ffirst",
+            "last_name": "Test llast",
+            "license_number": "ABB99999"
+        }
+        self.client.post(reverse("taxi:driver-create"), data=form_data)
+        new_user = get_user_model().objects.get(license_number=form_data["license_number"])
+
+        self.assertEqual(new_user.first_name, form_data["first_name"])
+        self.assertEqual(new_user.license_number, form_data["license_number"])
+
+
+class SearchFormTests(TestCase):
+    def test_search_driver_form_is_valid(self):
+        form_data = {
+            "username": "driver12"
+        }
+        form = DriverSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
